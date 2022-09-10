118a3a6866baec922297cbdeb01f7017c9ba815b
EmaPaul <enmanuelpaulcarrillo.rakata900@gmail.com>
2022-09-10 13:46:18 -0500

    vercel-config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 
 app.set('port',process.env.PORT || 3000);
 
@@ -7,6 +8,14 @@ app.use(express.json());
 
 app.use(require('./routes/product'))
 
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.listen(app.get('port'), ()=>{ 
     console.log("Server on port", app.get('port'))
