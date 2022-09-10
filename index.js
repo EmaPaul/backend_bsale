286ea05053d407ef576ee68045fa4dab7e7f12c2
EmaPaul <enmanuelpaulcarrillo.rakata900@gmail.com>
2022-09-10 13:55:06 -0500

    ajustes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port',process.env.PORT || 3000);
 
 app.use(express.json());
 
-app.use(require('./routes/product'))
+app.use(require('./src/routes/product'))
 
 app.use(cors());
 app.use((req, res, next) => {
